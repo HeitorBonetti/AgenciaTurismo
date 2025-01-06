58f234d2b6fe9a83268ce6c64522a6fd5fc26271
HeitorBonetti <heitor.bonetti@outlook.com>
2025-01-06 19:40:32 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <title>ClickTurismo</title>
+  </head>
+  <body>
+
+    <link href='https://fonts.googleapis.com/css?family=Poppins'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="#home">Início</a>
+  <a href="#travel">Destinos</a>
+  <a href="#contact">Contato</a>
+  <a href="#about">Sobre</a>
+</div>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="5000">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+  
+    <div class="carousel-inner">
+    <div class="item active">
+        <img src="https://www.dimoramarciana.com/images/public/slider/screen/8_1456157258.jpg" 
+        alt="Veneza">
+        <div class="carousel-caption">
+          <h3 class=clickc>Veneza</h3>
+          <p class=clickc>Passeie pela famosa Cidade Flutuante!</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="https://assets-global.website-files.com/63863c593fd9ef206df105ac/64503e39fb5b0952b3d1c427_JapanAnnouncement-Hero-1920x720.jpg" 
+        alt="Tokyo">
+        <div class="carousel-caption">
+          
+          <h3 class=clickc>Tokyo</h3> 
+          <p class=clickc>Visite a estética única Japonesa!</p>
+        </div>
+      </div>
+  
+      <div class="item">
+        <img src="https://cdn.paytour.com.br/assets/images/vitrines/30052023120834050c0c237a91b2394adecc12470b77d8.png"
+        alt="Rio">
+        <div class="carousel-caption">
+          <h3 class=clickc>Rio de Janeiro</h3>
+          <p class=clickc>Fotografe a paisagem da Cidade Maravilhosa!</p>
+        </div>
+      </div>
+    </div>
+  
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only"><</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">></span>
+    </a>
+  </div>
+
+<div class="jumbotron text-center" style="margin-bottom:50">
+</div>
+
+<section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card">
+            <img src="https://st2.depositphotos.com/1000921/5695/i/450/depositphotos_56952513-stock-photo-paris-house.jpg" class="card-img-top" alt="Destinos">
+            <div class="card-body">
+              <h5 class="card-title">Destinos em Alta</h5>
+              <a href="#sales" class="btn btn-primary">DESCUBRA</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="https://st.depositphotos.com/33766402/58781/i/450/depositphotos_587811026-stock-photo-kyiv-ukraine-11-06-2021.jpg" class="card-img-top" alt="Aluguel de Veículos">
+            <div class="card-body">
+              <h5 class="card-title">Aluguel de Veículos</h5>
+              <a href="#rental" class="btn btn-primary">DESCUBRA</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_600,q_auto,w_600//itemimages/12/46/1246784_v6.jpeg" class="card-img-top" alt="Aluguel de Hotéis">
+            <div class="card-body">
+              <h5 class="card-title">Hotéis e Estadia</h5>
+              <a href="#hotels" class="btn btn-primary">DESCUBRA</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container mt-5">
+      <h2 class="noti">Notícias</h2>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Vagas de Emprego</h5>
+              <p class="card-text">A ClickTurismo está com vagas de emprego em várias posições, mande ja seu currículo!</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Vôos para Ucrânia</h5>
+              <p class="card-text">Os vôos para Ucrânia, que antes eram proíbidos por conta do estado de guerra, voltaram ao ar!</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"> Voltando ao Normal</h5>
+              <p class="card-text">Após 2 anos, receita do turismo no mundo está perto do nível pré-pandemia.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</div>
+<footer>
+<div class="footer">
+
+  <div class="col-1">
+    <h3>LINKS</h3><br>
+    <a href="#about">Sobre</a>
+    <a href="#contact">Contato</a>
+    <a href="#forum">Fórum</a> 
+</div>
+
+<div class="col-10">
+<h3>NOTÍCIAS</h3>
+<form>
+  <input type="text" placeholder="E-Mail" class="erro">
+  <br>
+  <button type="submit" class="t12">INSCREVER-SE</button>
+</form>
+</div>
+
+<div class="col-1-1">
+<h3> CONTATO </h3><br>
+<p>Rua XYZ, 123<br>São Paulo, Brasil</p>
+
+
+<div class="socials">
+<li><a href="#facebook"><i-4x class="fa fa-facebook"></i></a>
+<a href="#twitter"><i-4x class="fa fa-twitter"></i></a>
+<a href="#instagram"><i-4x class="fa fa-instagram"></i></a>
+<a href="#youtube"><i-4x class="fa fa-youtube"></i></a>
+<a href="#linkedin"><i-4x class="fa fa-linkedin-square"></i></a></li>
+</div>
+</div>
+
+</footer>
+<div class="footer-bottom">
+            <p><a href="#privacy-policy">Política de Privacidade</a><br>&copy;2023 ClickTurismo - Todos os Direitos Reservados.</p>
+        </div>
+
+      </body>
+      </html>
